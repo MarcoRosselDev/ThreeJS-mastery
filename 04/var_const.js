8e480e4bde1080ff68c2390cb99b8ac96aa8213a
Marco Rossel <andresmarcorossel@gmail.com>
2024-12-03 02:35:30 -0300

    bruno simpons course | 09

---
+++ 04/var_const.js
@@ -0,0 +1,21 @@
+//1. Create a constant variable named entree and set it to equal to the string 'Enchiladas'.
+
+const entree = "Enchiladas";
+
+//2. Just to check that you’ve saved the value of 'Enchiladas' to entree, log the value of entree to the console.
+console.log(entree);
+
+/*3. Great, let’s see what happens if you try to reassign a constant variable.
+
+Paste the following code to the bottom of your program.
+
+entree = 'Tacos'
+
+to Clipboard
+
+This code throws the following error when you run your code:
+
+TypeError: Assignment to constant variable.
+After you clear this checkpoint, if you want to see about another quirk of const in action open the hint! */
+
+entree = "Tacos";
