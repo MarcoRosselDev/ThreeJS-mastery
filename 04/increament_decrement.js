28da6fb818793387c29e630c15015b9e11a93ef3
Marco Rossel <andresmarcorossel@gmail.com>
2024-12-05 02:21:21 -0300

    bruno simpons course | 10

---
+++ 04/increament_decrement.js
@@ -0,0 +1,10 @@
+let gainedDollar = 3;
+let lostDollar = 50;
+
+// 1. Using the increment operator, increase the value of gainedDollar.
+
+gainedDollar++;
+
+// 2. Using the decrement operator, decrement the value of lostDollar.
+
+lostDollar--;
---
+++ 04/string_concatenation.js
@@ -0,0 +1,15 @@
+// 1. Create a variable named favoriteAnimal and set it equal to your favorite animal.
+
+let favoriteAnimal = "Dog";
+
+// 2. Use console.log() to print 'My favorite animal: ANIMAL' to the console. Use string concatenation so that ANIMAL is replaced with the value in your favoriteAnimal variable.
+
+console.log("My favorite animal: " + favoriteAnimal);
+
+//  2.1 create a variable called myName and assign it your name.
+
+let myName = "Marco";
+
+//2.2 Create a variable called myCity and assign it your favorite city’s name.
+
+let myCity = "Valdivia";
