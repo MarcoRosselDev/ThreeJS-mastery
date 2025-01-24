64b51023977aa6904400b95abe4e19f9025cfe94
Marco Rossel <andresmarcorossel@gmail.com>
2025-01-23 22:07:16 -0300

    JS mastery unidada 3 | prototype 2

---
+++ JS-mastery/you-dont-know-JS/4-unit/1-alcance-y-cierre.js
@@ -0,0 +1,16 @@
+/* 
+https://www.mediafire.com/file/b7ahp5bljsa4qq4/1NV3NC1.S01E06.mkv/file
+https://www.mediafire.com/file/uhz04g8zvfpxs6c/1NV3NC1.S01E07.mkv/file
+https://www.mediafire.com/file/fs64gwwhnr90y7l/1NV3NC1.S01E08.mkv/file
+
+temporada 2
+
+https://www.mediafire.com/file/60b03e16dwj9uql/1NV3NC1.S02E01.mkv/file
+https://www.mediafire.com/file/o676hrkjy63t3hh/1NV3NC1.S02E02.mkv/file
+https://www.mediafire.com/file/zuk58dupoaafy8l/1NV3NC1.S02E03.mkv/file
+https://www.mediafire.com/file/efi7f7jml4n8t2b/1NV3NC1.S02E04.mkv/file
+https://www.mediafire.com/file/mly6bseicc0jj56/1NV3NC1.S02E05.mkv/file
+https://www.mediafire.com/file/jm0wl4yx9diyh0x/1NV3NC1.S02E06.mkv/file
+https://www.mediafire.com/file/womjicqj4ek5ay6/1NV3NC1.S02E07.mkv/file
+https://www.mediafire.com/file/u36u8kzddb5lk75/1NV3NC1.S02E08.mkv/file
+*/
