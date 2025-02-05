9cda41976328a2eaae5d02a8b56f8c94c9a29d7b
Marco Rossel <andresmarcorossel@gmail.com>
2025-02-05 05:44:26 -0300

    physics 4 | cannon

--- 11-physics/app.js
+++ 11-physics/app.js
@@ -1,4 +1,7 @@
 import * as THREE from "three";
+import CANNON from "cannon";
+
+console.log(CANNON);
 
 const size = {
   alto: window.innerHeight,
@@ -10,7 +13,9 @@ const mesh = new THREE.Mesh(cubo, material);
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, size.ancho / size.alto, 0.1, 40);
-camera.position.z = 5;
+//camera.position.z = 5;
+camera.position.set(2, 2, 2);
+camera.lookAt(mesh.position);
 
 scene.add(mesh);
 
--- 30-styles/app.js
+++ 30-styles/app.js
@@ -1,33 +1,23 @@
-// Syntax error from the start
+var students = [
+  { id: 14, name: "Kyle" },
+  { id: 73, name: "Suzy" },
+  { id: 112, name: "Frank" },
+  { id: 6, name: "Sarah" },
+];
 
-/* var greeting = "Hello";
-console.log(greeting);
-greeting = ."Hi";
-// SyntaxError: unexpected token . */
-
-/*  El navegador lanza un error antes de la ejecucion del programa
-por que greeting esta bien formulado
-y el console.log tambien.
-
-La unica explicacion para este comportamiento es que
-el programa primero se compila y analiza.
-*/
-
-// Early error ----- error temprano:
-
-console.log("Howdy");
-
-saySomething("Hello", "Hi");
-// Uncaught SyntaxError: Duplicate parameter name not
-// allowed in this context
-
-function saySomething(greeting, greeting) {
-  "use strict";
-  console.log(greeting);
+function getStudentName(studentID) {
+  for (let student of students) {
+    if (student.id == studentID) {
+      return student.name;
+    }
+  }
 }
 
-/* es use strict no permite que la funcion utilice nombres de parametro siguales,
-entre muchas otras cosas.
-pero como save JS que el use strict esta activado dentro de la funcion?
-solo se explica por la comliacion y analicis pervio a su ejecucion.
- */
+var nextStudent = getStudentName(73);
+
+console.log(nextStudent);
+// Suzy
+
+// target => source
+// left => right
+// objetivo => fuente
