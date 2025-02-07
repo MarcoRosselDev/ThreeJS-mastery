4f90a571a3b9421bdf60564922da6e41437befa1
Marco Rossel <andresmarcorossel@gmail.com>
2025-02-06 23:47:56 -0300

    physics 4 | gltf

---
+++ 31-gltf/index.js
@@ -0,0 +1,10 @@
+import * as THREE from "three";
+
+console.log("hola mundo");
+
+/* vamos a descargar modedlos de prueba de la pagina oficial de gltf,
+tambien veremos la alternativa draco: que es una version comprimida de los modelos.
+Draco es una alternativa buena si el modelo es pesado y tiene muchos detalles,
+pero requiere de mas recursos de calculo para descomprimir,
+pasandole esta carga de computo al usuario, lo que no es la mejor opcion.
+ */
